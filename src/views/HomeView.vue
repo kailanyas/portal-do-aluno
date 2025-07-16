c31afeed672dca4f86db68631017b41d736a7711
kailany <kailanyalves04@gmail.com>
2025-07-16 11:10:58 -0300

    feat: implementa a base da página inicial

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,50 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+import Cards from '../components/Cards.vue'
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+	<div class="layout-container">
+		<SideBar :isOpen="isSidebarOpen" @toggle="toggleSidebar"/>
+		<img src="../../public/imagens/logo-ufes.png" alt="Logo da UFES" class="logo-ufes"/>
+
+		<main class="main-content">
+			<h1>Conteúdo Principal</h1>
+			<p>Texto ou conteúdo da página.</p>
+		</main>
+
+  </div>
 </template>
+
+<script setup>
+	import {ref} from 'vue'
+	import SideBar from '../components/SideBar.vue'
+
+	const isSidebarOpen = ref(false)
+
+	function toggleSidebar() {
+		isSidebarOpen.value = !isSidebarOpen.value
+	}
+</script>
+
+<style>
+.layout-container{
+	display: flex;
+	height: 100vh;
+	width: 100%;
+	overflow: hidden;
+}
+
+.main-content{
+	flex: 1 1 auto;           
+	padding: 1.5rem;
+	overflow-y: auto;        
+	max-width: 100%;         
+	box-sizing: border-box;
+}
+
+.logo-ufes{
+  position: fixed;
+  top: 16px;
+  right: 16px;
+  height: 70px;
+  z-index: 1000;
+}
+</style>
