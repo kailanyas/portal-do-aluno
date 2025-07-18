7d077edd833918cde6ae0c0d2b5bd62fa456c722
Lukas Basilio <lukas.r.basilio@gmail.com>
2025-07-18 19:51:25 -0300

    Criação da Página de Solicitação de Atividades Complementares
    Criei a página de solicitação de atividades complementares e criei um componente (um card quadrado) pra poder colocar as informações dentro. Ficou faltando uns ajustes de design na página ainda que irei faze-los mais pra frente, mas a parte principal da página já esta pronta e funcionando.

---
+++ src/components/CardQuadrado.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="quadrado-branco-card">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.quadrado-branco-card {
+  width: 80%; 
+  height: 90%; 
+  background-color: white; 
+  border-radius: 6px; 
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1); 
+  
+  
+  display: flex;
+  flex-direction: column; 
+  justify-content: flex-start; 
+  align-items: flex-start; 
+  
+  padding: 30px; 
+  box-sizing: border-box; 
+  overflow-y: auto; 
+
+  
+  margin: 0; 
+}
+
+
+</style>
