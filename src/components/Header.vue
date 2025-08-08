0e4f7ef2137b5afa799f7b1a51d61baffd7c828d
Thiago Tonelli da Silva <thiagktonelli@gmail.com>
2025-08-08 11:08:28 -0300

    Melhorias para acessibilidade

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
   <header class="header">
     <div class="header-content">
       
-      <button v-if="showBackButton" @click="goBack" class="back-button">
+      <button v-if="showBackButton" @click="goBack" class="back-button" aria-label="botão de voltar para página anterior">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="24" height="24">
           <path d="M10.8284 12.0007L15.7782 16.9504L14.364 18.3646L8 12.0007L14.364 5.63672L15.7782 7.05093L10.8284 12.0007Z"></path>
         </svg>
